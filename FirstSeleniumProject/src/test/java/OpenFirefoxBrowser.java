b8ebca91aae71a3b8374d56619d162c9442c9b8e
ChopeyDmitry <46756756+ChopeyDmitry@users.noreply.github.com>
2019-01-24 20:33:23 +0200

    CW240119

--- FirstSeleniumProject/src/test/java/OpenFirefoxBrowser.java
+++ FirstSeleniumProject/src/test/java/OpenFirefoxBrowser.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,13 +17,37 @@ public class OpenFirefoxBrowser {
     }
     @Test
     public void testeBaySearch(){
-        wd.get("https://www.ebay.com/");
+        //open site
+        openSite();
+        type();
+        clickSearchButton();
+    }
+    @Test
+    public void testeBayFilter(){
+        openSite();
+        type();
+        clickSearchButton();
+        filterItemsByAuction();
+    }
+
+    public void filterItemsByAuction() {
+        wd.findElement(By.linkText("Auction")).click();
+    }
+
+    public void clickSearchButton() {
+        wd.findElement(By.id("gh-btn")).click();
+    }
+
+    public void type() {
         wd.findElement(By.id("gh-ac")).click();
         wd.findElement(By.id("gh-ac")).clear();
         wd.findElement(By.id("gh-ac")).sendKeys("glasses");
-
-        wd.findElement(By.id("gh-btn")).click();
     }
+
+    public void openSite() {
+        wd.get("https://www.ebay.com/");
+    }
+
     @AfterMethod
     public void tearDown(){
  //       wd.quit();
